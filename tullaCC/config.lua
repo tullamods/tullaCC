09fd9d16219b9e6a35e52138dfca63555083ad4a
Tuller <j85ason+git@gmail.com>
2010-08-30 17:10:41 -0400

    Renamed config to C, because L exists Added missing files from last commit

--- tullaCC/config.lua
+++ tullaCC/config.lua
@@ -1,23 +1,22 @@
 --[[
-	Confuration settings for tullaCC
+	Curation settings for tullaCC
 --]]
 
 
-local Conf = select(2, ...)
+local C = select(2, ...) --retrieve addon table
 
 --font settings
-Conf.fontFace = STANDARD_TEXT_FONT  --what font to use
-Conf.fontSize = 18  --the base font size to use at a scale of 1
-Conf.fontColor = {1, 0.92, 0}  --the minimum scale we want to show cooldown counts at, anything below this will be hidden
+C.fontFace = STANDARD_TEXT_FONT  --what font to use
+C.fontSize = 18  --the base font size to use at a scale of 1
 
 --display settings
-Conf.minScale = 0.6  --the minimum duration to show cooldown text for
-Conf.minDuration = 3 --the minimum number of seconds a cooldown must be to use to display in the expiring format
-Conf.expiringDuration = 5  --the minimum number of seconds a cooldown must be to use to display in the expiring format
+C.minScale = 0.6  --the minimum duration to show cooldown text for
+C.minDuration = 3 --the minimum number of seconds a cooldown must be to use to display in the expiring format
+C.expiringDuration = 5  --the minimum number of seconds a cooldown must be to use to display in the expiring format
 
 --text format strings
-Conf.expiringFormat = '|cffff0000%d|r' --format for timers that are soon to expire
-Conf.secondsFormat = '|cffffff00%d|r' --format for timers that have seconds remaining
-Conf.minutesFormat = '|cffffffff%dm|r' --format for timers that have minutes remaining
-Conf.hoursFormat = '|cff66ffff%dh|r' --format for timers that have hours remaining
-Conf.daysFormat = '|cff6666ff%dh|r' --format for timers that have days remaining
+C.expiringFormat = '|cffff0000%d|r' --format for timers that are soon to expire
+C.secondsFormat = '|cffffff00%d|r' --format for timers that have seconds remaining
+C.minutesFormat = '|cffffffff%dm|r' --format for timers that have minutes remaining
+C.hoursFormat = '|cff66ffff%dh|r' --format for timers that have hours remaining
+C.daysFormat = '|cff6666ff%dh|r' --format for timers that have days remaining
