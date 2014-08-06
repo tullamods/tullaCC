083ed216383ca69dbb02a46ff89694060078d1c3
Jason Greer <tuller@tullamods.com>
2014-08-06 19:45:15 -0400

    make config.lua more easy to override

--- config.lua
+++ config.lua
@@ -3,20 +3,59 @@
 --]]
 
 local AddonName, Addon = ...
-local C = {}; Addon.Config = C
+local _G = _G
 
---font settings
-C.fontFace = STANDARD_TEXT_FONT  --what font to use
-C.fontSize = 18  --the base font size to use at a scale of 1
+local defaults = {
+	--what font to use
+	fontFace = function() 
+		return _G['STANDARD_TEXT_FONT'] 
+	end,
+	
+	--the base font size to use at a scale of 1
+	fontSize = 18, 
 
---display settings
-C.minScale = 0.6 --the minimum scale we want to show cooldown counts at, anything below this will be hidden
-C.minDuration = 3 --the minimum number of seconds a cooldown's duration must be to display text
-C.expiringDuration = 5  --the minimum number of seconds a cooldown must be to display in the expiring format
+	--the minimum scale we want to show cooldown counts at, anything below this will be hidden
+	minScale = 0.6, 
+	
+	--the minimum number of seconds a cooldown's duration must be to display text
+	minDuration = 3, 
+	
+	--the minimum number of seconds a cooldown must be to display in the expiring format
+	expiringDuration = 5,  
 
---text format strings
-C.expiringFormat = '|cffff0000%d|r' --format for timers that are soon to expire
-C.secondsFormat = '|cffffff00%d|r' --format for timers that have seconds remaining
-C.minutesFormat = '|cffffffff%dm|r' --format for timers that have minutes remaining
-C.hoursFormat = '|cff66ffff%dh|r' --format for timers that have hours remaining
-C.daysFormat = '|cff6666ff%dh|r' --format for timers that have days remaining
+	--format for timers that are soon to expire
+	expiringFormat = '|cffff0000%d|r', 
+	
+	--format for timers that have seconds remaining
+	secondsFormat = '|cffffff00%d|r',
+	
+	--format for timers that have minutes remaining
+	minutesFormat = '|cffffffff%dm|r', 
+	
+	--format for timers that have hours remaining
+	hoursFormat = '|cff66ffff%dh|r', 
+	
+	--format for timers that have days remaining
+	daysFormat = '|cff6666ff%dh|r' 
+}
+
+Addon.Config = setmetatable({}, {
+	__index = function(t, k)
+		local value = defaults[k]
+
+		if value and type(value) == 'function' then
+			return value()
+		end		
+
+		return value
+	end
+})
+
+--make config editable
+do
+	local gAddon = _G[AddonName] or {}
+
+	gAddon.Config = Addon.Config	
+
+	_G[AddonName] = gAddon
+end
