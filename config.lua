094d40d778290d707a9cdbb2060d75de213cc642
Jason Greer <tuller@tullamods.com>
2014-10-14 21:31:47 -0400

    fixed typo and apparently reformatted whitespace

--- config.lua
+++ config.lua
@@ -7,36 +7,36 @@ local _G = _G
 
 local defaults = {
 	--what font to use
-	fontFace = function() 
-		return _G['STANDARD_TEXT_FONT'] 
+	fontFace = function()
+		return _G['STANDARD_TEXT_FONT']
 	end,
-	
+
 	--the base font size to use at a scale of 1
-	fontSize = 18, 
+	fontSize = 18,
 
 	--the minimum scale we want to show cooldown counts at, anything below this will be hidden
-	minScale = 0.6, 
-	
+	minScale = 0.6,
+
 	--the minimum number of seconds a cooldown's duration must be to display text
-	minDuration = 3, 
-	
+	minDuration = 3,
+
 	--the minimum number of seconds a cooldown must be to display in the expiring format
-	expiringDuration = 5,  
+	expiringDuration = 5,
 
 	--format for timers that are soon to expire
-	expiringFormat = '|cffff0000%d|r', 
-	
+	expiringFormat = '|cffff0000%d|r',
+
 	--format for timers that have seconds remaining
 	secondsFormat = '|cffffff00%d|r',
-	
+
 	--format for timers that have minutes remaining
-	minutesFormat = '|cffffffff%dm|r', 
-	
+	minutesFormat = '|cffffffff%dm|r',
+
 	--format for timers that have hours remaining
-	hoursFormat = '|cff66ffff%dh|r', 
-	
+	hoursFormat = '|cff66ffff%dh|r',
+
 	--format for timers that have days remaining
-	daysFormat = '|cff6666ff%dh|r' 
+	daysFormat = '|cff6666ff%dd|r' 
 }
 
 Addon.Config = setmetatable({}, {
@@ -45,7 +45,7 @@ Addon.Config = setmetatable({}, {
 
 		if value and type(value) == 'function' then
 			return value()
-		end		
+		end
 
 		return value
 	end
@@ -55,7 +55,7 @@ Addon.Config = setmetatable({}, {
 do
 	local gAddon = _G[AddonName] or {}
 
-	gAddon.Config = Addon.Config	
+	gAddon.Config = Addon.Config
 
 	_G[AddonName] = gAddon
 end
